9034ade26aeda87e461255fc330c732a543414e0
xmNuc <xmnuc@o2.pl>
2022-01-05 01:24:11 +0100

    3 commit

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ const { TodoRecord } = require('./records/todo.record');
 
 (async () => {
   try {
-    const todo = new TodoRecord({
-      title: 'Make MongoDB project',
-    });
-    await TodoRepository.insert(todo);
+    // const todo = new TodoRecord({
+    //   title: 'Make MongoDB project',
+    // });
+    // await TodoRepository.insert(todo);S
 
-    console.log(todo);
-    console.log(await TodoRepository.findAll());
+    // console.log(todo);
+    // console.log(await TodoRepository.findAll());
+
+    const todo = await TodoRepository.find('61d4d97c19756250c951b606');
+    console.log(await TodoRepository.find('61d4d97c19756250c951b606'));
+    todo.title = 'Updated information x 2';
+    await TodoRepository.update(todo);
+    console.log(await TodoRepository.find('61d4d97c19756250c951b606'));
+    console.log(await TodoRepository.delete(todo));
+    console.log(await TodoRepository.find('61d4d97c19756250c951b606'));
+    // console.log(await TodoRepository.find('61d4d96d6b60eca155edf6e4'));
   } finally {
     await client.close();
   }
--- repositories/todo.repository.js
+++ repositories/todo.repository.js
@@ -1,5 +1,6 @@
 const { TodoRecord } = require('../records/todo.record');
 const { todos } = require('../utils/db');
+const { ObjectId } = require('mongodb');
 
 class TodoRepository {
   static _checkRecord(record) {
@@ -19,9 +20,16 @@ class TodoRepository {
 
   static async delete(record) {
     TodoRepository._checkRecord(record);
+
+    await todos.deleteOne({
+      _id: record._id,
+    });
   }
 
-  static async find(id) {}
+  static async find(id) {
+    const item = await todos.findOne({ _id: ObjectId(String(id)) });
+    return item === null ? null : new TodoRecord(item);
+  }
 
   static async findAll() {
     return (await todos.find()).toArray();
@@ -29,6 +37,15 @@ class TodoRepository {
 
   static async update(record) {
     TodoRepository._checkRecord(record);
+
+    await todos.replaceOne(
+      {
+        _id: record._id,
+      },
+      {
+        title: String(record.title),
+      }
+    );
   }
 }
 
